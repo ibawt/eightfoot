9fb19f8dbff0cf2eec1b06d769845aeff63981f3
Anthony Cameron <anthony.cameron@jadedpixel.com>
2014-02-27 12:09:16 -0500

    display link to changed files on PRs

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,15 @@
 module IssuesHelper
+  def pull_request_url(issue)
+    pr = issue.try(:pull_request)
+
+    if pr.rels[:html]
+      return pr.rels[:html].href
+    else
+      nil
+    end
+  end
+
+  def pull_request_changes_url(pr_url)
+    return "#{pr_url}/files"
+  end
 end
