0c8cd89b5d7e2437069c2c93db373e0065295e46
Anthony Cameron <anthony.cameron@jadedpixel.com>
2014-02-27 14:01:23 -0500

    parse github usernames and turn them into links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       end
     end.html_safe if content.present?
   end
+
+  def userlinkify(content)
+    h(content).to_s.gsub(/(@[a-zA-Z0-9]+\w)/) do |match|
+      # todo: probably a good idea to check that the matched user is actually a real user on github, somehow..
+      "<a href='https://github.com/#{match.from(1)}' target='_blank'>#{match}</a>"
+    end.html_safe
+  end
 end
