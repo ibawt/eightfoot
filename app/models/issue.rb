5a0372445ef904c1e5898783a42b96c973f13d2f
Ian Quick <ian.quick@gmail.com>
2014-02-27 11:02:48 -0500

    temp fix for sizing.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,14 @@
 class Issue < ActiveRecord::Base
   belongs_to :project
   belongs_to :repository
+
+  def width
+    w = super
+    w.nil? ? 1 : w
+  end
+
+  def height
+    h = super
+    h.nil? ? 1 : h
+  end
 end
