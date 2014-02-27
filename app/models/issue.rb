91dda499cd25d7f0ca4dcc1cc8b6d48ab485edf5
Anthony Cameron <anthony.cameron@jadedpixel.com>
2014-02-27 11:34:14 -0500

    return 1 if gridster sizes are nil or 0

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,24 +4,26 @@ class Issue < ActiveRecord::Base
 
   def width
     w = super
-    w.nil? ? 1 : w
+    return 1 if w == 0 or w.nil?
+    w
   end
 
   def height
     h = super
-    h.nil? ? 1 : h
+    return 1 if h == 0 or h.nil?
+    h
   end
 
   def row
     r = super
-    return 1 if r == 0
-    r.nil? ? 1 : r
+    return 1 if r == 0 or r.nil?
+    r
   end
 
   def col
     c = super
-    return 1 if c == 0
-    c.nil? ? 1 : c
+    return 1 if c == 0 or c.nil?
+    c
   end
 
 end
