09eb340dc4f5896a2464095917548ae8fd436a04
Ian Quick <ian.quick@gmail.com>
2014-02-27 11:14:03 -0500

    ghetto fix

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -11,4 +11,17 @@ class Issue < ActiveRecord::Base
     h = super
     h.nil? ? 1 : h
   end
+
+  def row
+    r = super
+    return 1 if r == 0
+    r.nil? ? 1 : r
+  end
+
+  def col
+    c = super
+    return 1 if c == 0
+    c.nil? ? 1 : c
+  end
+
 end
