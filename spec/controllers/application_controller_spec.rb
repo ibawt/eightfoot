95bcb5f182567ad5d37382f4cf30e2388b9533f5
Ian Quick <ian.quick@gmail.com>
2014-03-06 09:35:47 -0500

    some more tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  login_user
+
   describe "github helper tests" do
     describe "prepare_github" do
       it "should use the TEST_API_TOKEN env variable if present" do
-        expect( ENV ).should receive(:[]).with('TEST_API_TOKEN').and_return('foobar')
-        expect( Octokit::Client).should receive(:new).with(:access_token => 'foobar')
+        expect( ENV ).to receive(:[]).with('TEST_API_TOKEN').and_return('foobar')
+        expect( Octokit::Client).to receive(:new).with(:access_token => 'foobar')
         controller.prepare_github
       end
-      it "should use the current_user's token if no env variable is present"
+      it "should use the current_user's token if no env variable is present" do
+        expect(Rails.env).to receive(:test?).and_return(false)
+        expect(Octokit::Client).to receive(:new).with(access_token: 'foobar-baz')
+        expect(controller.current_user).to receive(:token).and_return('foobar-baz')
+        controller.prepare_github
+      end
     end
 
     describe "github_client tests" do
