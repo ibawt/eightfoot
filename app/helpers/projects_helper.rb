5b09f628e2a2e95273ee6699aed2ce5f6976e9ee
Izaak Alpert <izaak.alpert@shopify.com>
2014-03-10 02:30:59 +0000

    null guard against blank columns

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,9 @@ module ProjectsHelper
   def is_repo_in_project?(repo)
     false
   end
+
+  def column_header(i)
+    @project.column_headers[i.to_s] ? @project.column_headers[i.to_s] : ''
+  end
+
 end
