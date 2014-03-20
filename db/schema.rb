26ea8fa8d7ee77f029acb7c7e2ac55e2a6cefba5
Anthony Cameron <anthony.cameron@jadedpixel.com>
2014-03-20 17:05:34 -0400

    remove unique index on email, since we need to be able to invite multiple github users who may have no email, and their nil email is colliding with others' (or we don't have permission to view)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307015502) do
+ActiveRecord::Schema.define(version: 20140320205346) do
 
   create_table "issues", force: true do |t|
     t.integer "repository_id"
